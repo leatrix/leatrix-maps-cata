261255b54e9fde1de90003bf287d7b2ce41b4508
leatrix <58888415+leatrix@users.noreply.github.com>
2022-12-01 03:56:50 +0000

    3.0.63.alpha.1

--- Leatrix_Maps.lua
+++ Leatrix_Maps.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- 	Leatrix Maps 3.0.62 (30th November 2022)
+	-- 	Leatrix Maps 3.0.63.alpha.1 (1st December 2022)
 	----------------------------------------------------------------------
 
 	-- 10:Func, 20:Comm, 30:Evnt, 40:Panl
@@ -12,7 +12,7 @@
 	local LeaMapsLC, LeaMapsCB, LeaDropList, LeaConfigList = {}, {}, {}, {}
 
 	-- Version
-	LeaMapsLC["AddonVer"] = "3.0.62"
+	LeaMapsLC["AddonVer"] = "3.0.63.alpha.1"
 
 	-- Get locale table
 	local void, Leatrix_Maps = ...
@@ -77,6 +77,9 @@
 		-- Hide right-click to zoom out button and message
 		WorldMapZoomOutButton:Hide()
 		WorldMapMagnifyingGlassButton:Hide()
+
+		-- Set map frame strata to ensure other windows can interleave with it
+		WorldMapFrame:SetFrameStrata("MEDIUM")
 
 		----------------------------------------------------------------------
 		-- Show zone dropdown menu
