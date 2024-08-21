1611868d9de61efb162eeb8ac5db1c8da92d337f
leatrix <support@leatrix.com>
2024-08-21 08:47:33 +0100

    4.0.21

--- Leatrix_Maps.lua
+++ Leatrix_Maps.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- 	Leatrix Maps 4.0.21.alpha.1 (14th August 2024)
+	-- 	Leatrix Maps 4.0.21 (21st August 2024)
 	----------------------------------------------------------------------
 
 	-- 10:Func, 20:Comm, 30:Evnt, 40:Panl
@@ -12,7 +12,7 @@
 	local LeaMapsLC, LeaMapsCB, LeaDropList, LeaConfigList, LeaLockList = {}, {}, {}, {}, {}
 
 	-- Version
-	LeaMapsLC["AddonVer"] = "4.0.21.alpha.1"
+	LeaMapsLC["AddonVer"] = "4.0.21"
 
 	-- Get locale table
 	local void, Leatrix_Maps = ...
